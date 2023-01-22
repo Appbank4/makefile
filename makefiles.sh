2816e6265efffbab9d635c430e9f3f7f02a0ba86
Appbank4 <118092670+Appbank4@users.noreply.github.com>
2023-01-22 12:01:02 -0500

    Update makefiles.sh

--- makefiles.sh
+++ makefiles.sh
@@ -5,3 +5,5 @@ touch $filename
 echo "Welcome to Etech Consulting" >> $filename
 sudo chmod +x $filename
 echo "Thank you for complying with runbook policy $filename 
+ll $(pwd) | grep $filename
+cp $filename /tmp
