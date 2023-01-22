436299896c18a5572c30eb08b411b152ac97645c
appbank4 <appbank22@gmail.com>
2023-01-22 11:07:29 -0500

    file added
    Signed-off-by: appbank4 <appbank22@gmail.com>

---
+++ makefiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo -n "Please enter the file name: "
+read filename
+touch $filename
+echo "Welcome to Etech Consulting" >> $filename
+sudo chmod +x $filename
+echo "Thank you for complying with runbook policy $filename 
